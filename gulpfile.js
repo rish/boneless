31651fb3d877106a45aab2950bd831fc39928f07
Rish <rish@users.noreply.github.com>
2015-04-05 10:09:20 +0100

    Add jade task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var jade = require('gulp-jade');
 
 gulp.task('default', function() {
   console.log("Gulp it up!!!");
+  browserSync({
+    server: {
+      baseDir: "build/"
+    }
+  });
+});
+
+gulp.task('jade', function() {
+  gulp.src('app/views/**/*.jade')
+    .pipe(jade({ pretty: true}))
+    .pipe(gulp.dest('build'));
 });
